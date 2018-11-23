583c92d1e86941047e30d7286a4e51f0a2f3fc70
Winnie <winnierjren@gmail.com>
2018-11-23 13:17:49 -0500

    poster showing

---
+++ Movie-Tickets-App/src/moviePoster.js
@@ -0,0 +1,84 @@
+import React, { Component, PropTypes } from "react";
+import {
+  Dimensions,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from "react-native";
+import { defaultStyles } from "./styles";
+
+const { width, height } = Dimensions.get("window");
+//screen dimentions
+
+const cols = 3,
+  rows = 3;
+//posters in each row and col
+
+export default class MoviePoster extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    //this.handleClick = this.handleClick.bind(this);
+  }
+
+  //   handleClick = () => {
+  //       this.setState = {
+  //           popUpIsOpen = true
+  //       }
+  //   };
+
+  // Component prop types
+
+  render() {
+    const {
+      movie,
+      movie: { title, genre, poster },
+      onOpen
+    } = this.props;
+    return (
+      <TouchableOpacity
+        style={styles.container}
+        //onPress={() => this.handleClick(movie)}
+      >
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: poster }} style={styles.image} />
+        </View>
+        <Text style={styles.title} numberOfLines={1}>
+          {title}
+        </Text>
+        <Text style={styles.genre} numberOfLines={1}>
+          {genre}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+    marginBottom: 10,
+    height: (height - 20 - 20) / rows - 10,
+    width: (width - 10) / cols - 10
+  },
+  imageContainer: {
+    flex: 1 // take up all available space
+  },
+  image: {
+    borderRadius: 10, // rounded corners
+    ...StyleSheet.absoluteFillObject // fill up all space in a container
+  },
+  title: {
+    ...defaultStyles.text,
+    fontSize: 14,
+    marginTop: 4
+  },
+  genre: {
+    ...defaultStyles.text,
+    color: "#BBBBBB",
+    fontSize: 12,
+    lineHeight: 14
+  }
+});
--- Movie-Tickets-App/src/movies.js
+++ Movie-Tickets-App/src/movies.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import { ScrollView, Text, View } from "react-native";
 import { movies } from "./data";
+import MoviePoster from "./moviePoster";
 
 export default class Movies extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    //this.handleClick = this.handleClick.bind(this);
+  }
   render() {
     return (
       <View>
         <ScrollView>
           {movies.map((movie, index) => (
-            <Text>{movie.title}</Text>
+            <MoviePoster
+              movie={movie}
+              //onPress={this.props.handleClick}
+              key={index}
+            />
           ))}
         </ScrollView>
       </View>
